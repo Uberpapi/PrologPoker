ff694a097ac1e678f5d254bf14dceae60e515e37
Daniel <b.daniel.karlsson@gmail.com>
2017-09-26 13:46:30 +0200

    listing of deals

--- dealer.pl
+++ dealer.pl
@@ -1,6 +1,22 @@
 ﻿
-:-module(dealer,[dealtp/4,dealflop/3,dealturn/3, dealriver/2,deck/1]).
+:-module(dealer,[dealtp/4, dealflop/3,
+                  dealturn/3, dealriver/2,
+                  createDeck/1,
+                  setListDeck/1, setP1card/1,
+                  setP2card/1, setFlop/1,
+                  setTurn/1, setRiver/1, deck/1,
+                  player1/1, player2/1, flop/1,
+                  turn/1, river/1]).
+
 :-use_module(library(random)).
+
+:- dynamic deck/1.
+:- dynamic player1/1.
+:- dynamic player2/1.
+:- dynamic flop/1.
+:- dynamic turn/1.
+:- dynamic river/1.
+
 
 % There are 4 different colors
 color(clubs).
@@ -24,16 +40,24 @@ value('Q').
 value('K').
 
 % dealtp(+, -, -, -).
-dealtp([C1,C2,C3,C4|Deck], [C1,C3], [C2,C4], Deck).
+dealtp([C1,C2,C3,C4|Deck], [C1,C3], [C2,C4], Deck) :-
+  setP1card([C1, C3]),
+  setP2card([C2, C4]),
+  setListDeck(Deck).
 
 % dealflop(+, -, -).
-dealflop([_,C2,C3,C4|Deck],[C2,C3,C4], Deck).
+dealflop([_,C2,C3,C4|Deck],[C2,C3,C4], Deck) :-
+  setFlop([C2, C3, C4]),
+  setListDeck(Deck).
 
 % dealturn(+, -, -).
-dealturn([_,C2|Deck], [C2], Deck).
+dealturn([_,C2|Deck], [C2], Deck) :-
+  setTurn([C2]),
+  setListDeck(Deck).
 
 % dealriver(+, -).
-dealriver([_,C2|_],[C2]).
+dealriver([_,C2|_],[C2]) :-
+  setRiver([C2]).
 
 % A card is defined by 2 terms, Color and Value
 % card(-, -).
@@ -43,11 +67,43 @@ card(Color, Value):-
 
 % Create a deck then shuffle it.
 % deck(-).
-deck(Shuffled):-
-  createDeck(Unshuffled),
-  random_permutation(Unshuffled,Shuffled).
+createDeck(Shuffled):-
+  findall(card(X, Y), card(X, Y), L),
+  random_permutation(L, Shuffled).
 
-% A list containing all the cards
-% createDeck(-).
-createDeck(L):-
-  findall(card(X, Y), card(X, Y), L).
+setListDeck(_) :-
+  retract(deck(_)),
+  fail.
+setListDeck(X) :-
+  assert(deck(X)).
+
+setP1card(_) :-
+  retract(player1(_)),
+  fail.
+
+setP1card(X) :-
+  assert(player1(X)).
+
+setP2card(_) :-
+  retract(player2(_)),
+  fail.
+setP2card(X) :-
+  assert(player2(X)).
+
+setFlop(_) :-
+  retract(flop(_)),
+  fail.
+setFlop(X) :-
+  assert(flop(X)).
+
+setTurn(_) :-
+  retract(turn(_)),
+  fail.
+setTurn(X) :-
+  assert(turn(X)).
+
+setRiver(_) :-
+  retract(river(_)),
+  fail.
+setRiver(X) :-
+  assert(river(X)).
--- game.pl
+++ game.pl
@@ -1,11 +1,22 @@
 
-:-module(game,[game/5]).
+:-module(game,[play/0, done/0, go/0]).
 :-use_module(dealer).
 
-% game(-, -, -, -, -). 
-game(P1,P2,Flop,Turn,River):-
-  deck(Shuffled),
-  dealtp(Shuffled, P1 , P2, Restfromtp),
-  dealflop(Restfromtp, Flop, Restfromflop),
-  dealturn(Restfromflop, Turn, Restfromturn),
-  dealriver(Restfromturn, River).
+p1 :- listing(player1).
+p2 :- listing(player2).
+flop :- listing(flop).
+turn :- listing(turn).
+river :- listing(river).
+
+play :-
+  write('>> '),
+  read(X),
+  call(X),
+play.
+
+go :-
+  createDeck(X),
+  dealtp(X, P1, P2, Y),
+  dealflop(Y, Flop, W),
+  dealturn(W, Turn, Z),
+  dealriver(Z, River).
